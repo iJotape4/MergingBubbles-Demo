207e24e2a278e30a495a7c9ee86b586945cdd146
JotaPe <jcamacho249@unab.edu.co>
2024-03-10 10:56:57 -0500

    [Add] Bubbles Merging behavior

---
+++ Assets/Game Elements/Bubble/Scripts/BubbleBehavior.cs
@@ -0,0 +1,65 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class BubbleBehavior : MonoBehaviour
+{
+    [HideInInspector] const string bubbleTag = "Bubble";
+
+    [SerializeField] private float _currentBubbleSize = 1f;
+    [SerializeField] private float mergeTime = 0.5f;
+
+    public float CurrentBubbleSize
+    {
+        get { return _currentBubbleSize; }
+        private set { _currentBubbleSize = value; }
+    }
+
+    public BubbleBehavior(float currentBubbleSize)
+    {
+        this._currentBubbleSize = currentBubbleSize;
+    }
+
+    private void Awake()
+    {
+        SetBubbleSize();
+    }
+
+    void SetBubbleSize()
+    {
+        transform.localScale = Vector3.one * _currentBubbleSize;
+    }
+
+    public void MergeWithOtherBubble(float otherBubbleSize)
+    {
+        if(otherBubbleSize < _currentBubbleSize) 
+        { 
+            float newBubbleSize = _currentBubbleSize + otherBubbleSize;
+            transform.DOScale(newBubbleSize, mergeTime);        
+        }
+        else if(otherBubbleSize > _currentBubbleSize)
+        {
+            float newBubbleSize = 0;
+            transform.DOScale(newBubbleSize, mergeTime).OnComplete(() => DestroyBubble());
+        }
+        else if(_currentBubbleSize == otherBubbleSize) 
+        {
+            Debug.Log("Bubbles has an equal size, can't be merged");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == bubbleTag)
+        {
+            BubbleBehavior otherBubble = collision.transform.GetComponent<BubbleBehavior>();
+            MergeWithOtherBubble(otherBubble.CurrentBubbleSize);
+            otherBubble.MergeWithOtherBubble(_currentBubbleSize);
+        }
+    }
+
+    void DestroyBubble()
+    {
+        //TODO: add VFX  and more
+        Destroy(this.gameObject);
+    }
+}
